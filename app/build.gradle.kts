0a07a2c2cda3961a2b80b3a998432e4764014742
SeuNome <seu.email@example.com>
2024-03-04 22:39:56 -0300

    Mensagem do commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,12 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation ("androidx.compose.material3:material3:1.2.0")
+    implementation ("androidx.compose.ui:ui:1.6.2")
+    implementation ("androidx.activity:activity-compose:1.8.2")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/br/com/fiap/listaslazy/MainActivity.kt
+++ app/src/main/java/br/com/fiap/listaslazy/MainActivity.kt
@@ -10,12 +10,16 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.fiap.listaslazy.model.Game
 import br.com.fiap.listaslazy.repository.getAllGames
+import br.com.fiap.listaslazy.repository.getGamesByStudio
 import br.com.fiap.listaslazy.ui.theme.ListasLazyTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,6 +42,14 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GamesScreen() {
+    var searchTextState by remember {
+        mutableStateOf<String>("")
+    }
+
+    var gamesListState by remember {
+        mutableStateOf<List<Game>>(getAllGames())
+    }
+
     Column(modifier = Modifier.padding(16.dp)) {
         Text(
             text = "Meus jogos favoritos",
@@ -45,28 +57,74 @@ fun GamesScreen() {
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn() {
+            items(gamesListState) {
+                GameCard(game = it)
+            }
+        }
+
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = searchTextState,
+            onValueChange = {
+                searchTextState = it
+            },
             modifier = Modifier.fillMaxWidth(),
             label = {
                 Text(text = "Nome do estúdio")
             },
             trailingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Default.Search,
-                        contentDescription = ""
-                    )
+                IconButton(onClick = {
+                    gamesListState = getGamesByStudio(searchTextState)
+                }) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "")
                 }
             }
         )
+
         Spacer(modifier = Modifier.height(16.dp))
-        LazyColumn {
+
+        LazyColumn() {
             items(getAllGames()) { game ->
-                Column {
-                    Text(text = game.title)
-                }
+                GameCard(game = game)
+            }
+        }
+    }
+}
+
+@Composable
+fun GameCard(game: Game) {
+    Card(modifier = Modifier.padding(bottom = 8.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .weight(3f)
+            ) {
+                Text(
+                    text = game.title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = game.studio,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                )
+                Text(
+                    text = game.releaseYear.toString(),
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth(),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF0000FF) // Cor azul
+                )
             }
         }
     }
